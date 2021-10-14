bf017ffbbfadf9a246ec8a7b4ca17c8e5a714d16
unknown <omar.hija12>
2021-10-14 16:27:28 +0300

    full Login page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,101 +1,36 @@
 import React from 'react'
 import {Link} from "react-router-dom"
-
 import styled from 'styled-components'
+import {Container,Nave,Logo,Rigttbtn,Join,Signin,Hero,LeftSide,Links} from './Style'
 export default function Login() {
     return (
 
     <Container>
-        <Nav>
+        < Nave >
         <Logo>
             <Link to="/"><img src="Img/loginlogo.svg" alt="Logo LinkedIn"></img></Link>
         </Logo>
-        <div>
+        <Rigttbtn>
         <Join><Link to="/SignUp">Join Now</Link></Join>
-        <SignIn><Link to="/SignIn">Sign in</Link></SignIn>
-        </div>
-        </Nav>
-<Section>
-    <PotrLogin>
-    <h1>Welcome to Your Profeddional Community</h1>
-    <img src="Img/loginhero.svg" alt="Img Section "></img>
-    </PotrLogin>
-    <Google>
-        <img src="" alt="google icon"></img>
-        <span>Sign in With Google</span>
-    </Google>
-</Section>
-    </Container>
-    )
-}
- const Container =styled.div`
- padding:0;
- background-color:#FFFFFF;
+        <Signin><Link to="/SignIn">Sign in</Link></Signin>
+        </Rigttbtn>
+        </Nave>
 
- `;
- const Nav =styled.div`
-     padding: 10px 10%;
-     display: flex;
-     align-items: center;
-     justify-content: space-between;
-     margin: 0 15px;
-     flex-wrap: nowrap;
-     font-weight:600;
-     @media (max-width:480px){
-         padding: 15px 0;
-         font-weight: 300;
- }
+ <Hero>
+    <LeftSide >
+    <h1>Welcome to Your Professional Community</h1>
+  
+   <Links>
+       <a href="##" ><li href="##">Search For a job</li><i className="fas fa-chevron-right"></i></a>
+       <a  href="##"><li href="##">Find a Person You Know</li><i className="fas fa-chevron-right"></i></a>
+       <a  href="##"><li href="##">Learn a new skill</li><i className="fas fa-chevron-right"></i></a>
+       
+   </Links>
+    </LeftSide>
+    <img className="bigImg" src="Img/SectionImg.svg" alt="Img Section "></img>
 
-
- `;
- const Logo=styled.nav `
- width:130px;
- @media (max-width:480px){
-     width: 70px;
- }
- & > img{
-     width: 100%;
- }
- `;
- const Join=styled.span`
-     display: inline-block;
-     margin-right: 10px;
-     padding: 15px 10px;
-     cursor: pointer;
- & a {
-     text-decoration: none;
-     color:#9e9e9e;
- }
-     &:hover{
-         background-color: #EBEBEB;
-         border-radius:4px
-     }
-     `;
- const SignIn=styled.span `
-     padding: 10px 20px;
-     cursor: pointer;
-     border:1px #0073b1 solid;
-     border-radius: 34px;
-     width: 50%;
-     text-align: center;
-     &:hover{
-     background-color: rgba(112,181,249,0.15);
-     color: red;
-     box-shadow:0 0 0 2px #0a66c2;
-     }
- & >a{
-     text-decoration: none;
-     color:#0073b1;
- }
- @media (max-width:480px){
-     padding: 5px 10px;
- }
- `;
- const Section=styled.div`
- display: flex;
- flex-wrap:wrap;
- margin-top: 40px;
- `;
- const PotrLogin=styled.div`
- `
- const Google =styled.div``
+   </Hero> 
+ 
+</Container>
+)}
+  
--- src/Components/Login/Style.js
+++ src/Components/Login/Style.js
@@ -1,4 +1,198 @@
 
+import styled from "styled-components"
+
+export const Container=styled.div`
+    max-width: 79%;
+    width: 79%;
+    margin: auto;
+    margin-top: 16px;
+
+    @media (max-width:960px){
+    width: 96%;
+    max-width: 96%;
+    margin: 10px auto;
+    }
+
+    @media(max-width:760px){//
+        width: 96%;
+    max-width: 96%;
+    margin: 10px auto ;
+    }
 
 
+ 
+`
+
+export const Nave=styled.div`
+  
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: nowrap;
+    align-items: center;
+`
+
+export const Logo=styled.div`
+    width: 133px;
+
+    @media(max-width:760px){
+        width:15%
+    }
+`
+
+export const Rigttbtn=styled.div`
+    display: flex;
+    justify-content: space-between;
+   
+`
+
+export const Join=styled.div`
+    margin-right: 15px;
+    padding: 0px 15px;
+    display: flex;
+    align-items: center;
+
+    & a{
+    text-decoration: none;
+    color: #a6a6a6;
+    font-weight: 600;
+
+    @media (max-width:460px){
+    text-decoration: none;
+    color: #a6a6a6;
+    font-weight: bold;
+    font-size: 0.80rem;
+}
+}
+
+   :hover{
+background-color: #ededed;
+    border-radius: 4px;
+    }
+
+    @media(max-width:760px){
+        margin-right: 5px;
+        padding: 10px 15px;
+        font-size:0.80rem
+
+    }
+`
+ 
+
+export const Signin=styled.div`
+    padding: 10px 22px;
+    border: 1px solid #0A66C2;
+    border-radius: 30px;
+
+    :hover{
+     background-color: rgba(112, 181, 249, 0.15);
+     color: #0a66c2;
+    }
+
+    & a{
+    text-decoration: none;
+    color: #0a66c2;
+    font-weight: bold;
+    }
+
+    @media(max-width:760px){
+             font-size:0.80rem;
+        padding: 7px 10px;
+    }
+
+    @media (max-width:460px){
+        padding: 3px 4px;
+        font-size: 0.8rem;}
+`
+
+
+export const Hero=styled.div`
+    display: flex; 
+    justify-content: space-between;
+    align-items: flex-start;
+    position: absolute;
+  margin-top: 80px;
+  height:auto;
+
+  & img {
+    width:40%;
+    flex-shrink: 2;
+    align-self: end;
+
+    @media (max-width:760px){
+    align-self: center;
+    margin-top: 30px;
+    width: 80%;}
+}
+
+@media (max-width:760px){
+  flex-direction: column;
+}
+`
+
+
+export const LeftSide=styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-right: 15px;
+    justify-content: space-between;
+
+    & h1{
+    font-size: 2.5rem;
+    font-weight: 100;
+    color:#8f5849;
+    line-height:1.5;
+
+    @media (max-width:760px){
+   font-size: 1rem;
+   text-align: center;
+    }
+}
+  
+@media (max-width:760px){
+width: 96%;
+}
+`
+
+export const Links=styled.div`
+    margin-top: 16px;
+
+    & a  {
+    display: flex;
+    justify-content: space-between;
+    width: 420px;
+    padding: 18px 20px;
+    margin-top: 16px;
+    text-decoration: none;
+    border: 1px #c5c5c5 solid;
+    border-radius: 7px;
+    color:black;
+
+  :hover{
+  box-shadow:2px 4px 10px 1px rgb(0 0 0 / 30%);
+    }
+
+
+    & li{
+    list-style: none;
+    font-size: 18px;
+    }
+
+    @media (max-width:760px){
+        width: 96%;
+        margin: auto;
+        margin-top: 16px;
+        font-size:1rem
+    }
+}
+
+
+
+
+& ul a li{
+@media (max-width:760px){
+font-size: 16px;
+}
+}
+`
 
